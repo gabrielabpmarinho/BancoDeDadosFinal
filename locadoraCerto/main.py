cff247b96dcbba666dc09dbb0a5c1edf2096d54f
gabriela <gvbpaiva@gmail.com>
2021-05-05 22:45:27 -0300

    colocado o  executemany na função execute. e feito as funcoes insert,delete,update, select

--- locadoraCerto/main.py
+++ locadoraCerto/main.py
@@ -6,7 +6,7 @@ params = None
 def execute (sql,params = None): # serve para insert,update,delet,create,alter,drop
     with connect(host="localhost", user ="root", password = "ga2468bi", database="locadora") as conn:
         with conn.cursor() as cursor:
-            cursor.execute(sql,params)
+            cursor.executemany(sql,params)# colocando executemany aqui não é necessario a estrutura for nas funçoes q usam execute
             conn.commit()
 
 def query(sql,params = None):# executa comando e retorna resultado
@@ -15,3 +15,17 @@ def query(sql,params = None):# executa comando e retorna resultado
             cursor.execute(sql,params)
             return cursor.fetchall() # qnd vou fazer o select tem q retornar algo, ou seja pega os valores q o bd retornou
 
+def insert(tabela, colunas, valores):
+     execute(f"INSERT INTO {tabela} ({','.join(colunas)}) VALUES ({','.join('%s')})", valores)
+
+
+def delete(tabela, coluna, valor):
+    execute(f"DELETE FROM {tabela} WHERE {coluna} = %s", (valor,))
+
+
+def update(tabela, chave, valor_chave, colunas, valores):
+    sets = [f"{coluna} = %s" for coluna in colunas]
+    execute(f"""UPDATE {tabela} SET {",".join(sets)} WHERE {chave} = %s""", valores + [valor_chave])
+
+def select(tabela, chave, valor_chave):
+    query(f"SELECT * FROM {tabela} WHERE {chave} =%s", (valor_chave,)) #para retornar um valor sempre é usado o query
